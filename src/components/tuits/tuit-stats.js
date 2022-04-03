5f2a6f948bbe9f59b66dd97a1949e2e958d7394a
Pavithraa <pavithraapanch@gmail.com>
2022-04-02 22:35:40 -0400

    Adding Unit Test files

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -2,30 +2,30 @@ import React from "react";
 
 const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
     return (
-      <div className="row mt-2">
-        <div className="col">
-          <i className="far fa-message me-1"></i>
-          {tuit.stats && tuit.stats.replies}
-        </div>
-        <div className="col">
-          <i className="far fa-retweet me-1"></i>
-          {tuit.stats && tuit.stats.retuits}
-        </div>
-        <div className="col">
-          <span onClick={() => likeTuit(tuit)}>
+        <div className="row mt-2">
+            <div className="col">
+                <i className="far fa-message me-1"></i>
+                {tuit.stats && <span className="ttr-stats-replies">{tuit.stats.replies}</span>}
+            </div>
+            <div className="col">
+                <i className="far fa-retweet me-1"></i>
+                {tuit.stats && <span className="ttr-stats-retuits">{tuit.stats.retuits}</span>}
+            </div>
+            <div className="col">
+          <span className="ttr-like-tuit-click" onClick={() => likeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                  tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
                   <i className="fas fa-thumbs-up me-1" style={{color: 'blue'}}></i>
               }
               {
-                tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
                   <i className="fas fa-thumbs-up me-1"></i>
               }
-            {tuit.stats && tuit.stats.likes}
+              <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
           </span>
-        </div>
-        <div className="col">
-          <span onClick={() => dislikeTuit(tuit)}>
+            </div>
+            <div className="col">
+          <span className="ttr-dislike-tuit-click" onClick={() => dislikeTuit(tuit)}>
               {
                   tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
                   <i className="fas fa-thumbs-down me-1" style={{color: 'blue'}}></i>
@@ -34,13 +34,13 @@ const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
                   tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes <= 0 &&
                   <i className="fas fa-thumbs-down me-1"></i>
               }
-              {tuit.stats && tuit.stats.dislikes}
+              <span className="ttr-stats-dislikes">{tuit.stats && tuit.stats.dislikes}</span>
           </span>
+            </div>
+            <div className="col">
+                <i className="far fa-inbox-out"></i>
+            </div>
         </div>
-          <div className="col">
-          <i className="far fa-inbox-out"></i>
-        </div>
-      </div>
     );
 }
 export default TuitStats;
---
+++ src/components/tuits/tuit-stats.test.js
@@ -0,0 +1,72 @@
+import {act, create} from 'react-test-renderer';
+import TuitStats from "./tuit-stats";
+
+test('stats render correctly', () => {
+    let stats = {
+        likes: 123,
+        replies: 234,
+        retuits: 345,
+        dislikes: 12
+    }
+
+    const likeTuit = () => {
+        act(() => {
+            stats.likes++;
+            tuitStats.update(
+                <TuitStats
+                    tuit={{stats: stats}}
+                    likeTuit={() => {}}
+                />)
+        })
+    }
+
+    const dislikeTuit = () => {
+        act(() => {
+            stats.dislikes++;
+            tuitStats.update(
+                <TuitStats
+                    tuit={{stats: stats}}
+                    dislikeTuit={() => {}}
+                />)
+        })
+    }
+
+    let tuitStats
+    act(() => {
+        tuitStats = create(
+            <TuitStats
+                likeTuit={likeTuit}
+                tuit={{stats: stats}}
+                dislikeTuit={dislikeTuit}
+            />
+        );
+    })
+
+    const root = tuitStats.root;
+    const likesCounter = root.findByProps({className: 'ttr-stats-likes'})
+    console.log("likesCounter",likesCounter);
+    const dislikesCounter = root.findByProps({className: 'ttr-stats-dislikes'})
+    const repliesCounter = root.findByProps({className: 'ttr-stats-replies'})
+    const retuitsCounter = root.findByProps({className: 'ttr-stats-retuits'})
+
+    let likesText = likesCounter.children[0];
+    const repliesText = repliesCounter.children[0];
+    const retuitsText = retuitsCounter.children[0];
+    let dislikesText = dislikesCounter.children[0];
+    expect(likesText).toBe('123');
+    expect(repliesText).toBe('234');
+    expect(retuitsText).toBe('345');
+    expect(dislikesText).toBe('12');
+
+    const likeTuitButton = root.findByProps(
+        {className: 'ttr-like-tuit-click'})
+    act(() => {likeTuitButton.props.onClick()})
+    likesText = likesCounter.children[0];
+    expect(likesText).toBe('124');
+
+    const dislikeTuitButton = root.findByProps(
+        {className: 'ttr-dislike-tuit-click'})
+    act(() => {dislikeTuitButton.props.onClick()})
+    dislikesText = dislikesCounter.children[0];
+    expect(dislikesText).toBe('13');
+});
